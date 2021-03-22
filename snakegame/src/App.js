b6e754f4aeeddc6d9e13eb8d14f9669a53e164f8
jayanandhana <41743804+jayanandhana@users.noreply.github.com>
2021-03-22 12:33:21 +0530

    Add files via upload

---
+++ snakegame/src/App.js
@@ -0,0 +1,11 @@
+import SnakeGame from "./SnakeGame"
+import React from 'react';
+// const { whyDidYouUpdate } = require('why-did-you-update');
+// whyDidYouUpdate(React);
+function App() {
+  return (
+    <SnakeGame />
+  );
+}
+
+export default App;
